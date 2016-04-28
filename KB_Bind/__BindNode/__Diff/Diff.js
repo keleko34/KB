8a1b88c45b59fc6c053a3f84a9fe1a2076a018b8
keleko34 <gguidero@dataart.com>
2016-04-28 14:48:12 +0200

    added bind compat

--- KB_Bind/__BindNode/__Diff/Diff.js
+++ KB_Bind/__BindNode/__Diff/Diff.js
@@ -12,32 +12,138 @@ define([],function(){
       return Diff.diffString(o,n);
     }
 
-    Diff.convertWordArrToObj = function(arr,otherStr)
+    //iterate word array ex ['word','word2','word3'] to object: {word:{rows:[{pos:0,bind:undefined}]},word2:{rows:[{pos:1,bind:undefined}]},word3:{rows:[{pos:2,bind:undefined}]}}
+    // if word in array has binding it will be format as {value:func,bind:func}, bind is passed along to the bind for the position
+    Diff.convertWordArrToObj = function(arr)
     {
+      var currValue = '',
+          currBind;
       return arr.reduce(function(o,v,i){
-       o[v] = (o[v] === undefined ? Object.defineProperty({rows:[i]},otherStr,{value:null,writable:true}) : (o[v].rows.push(i) ? o[v] : Object.defineProperty({rows:[i]},otherStr,{value:null,writable:true})));
+        currValue = (typeof v.value === 'function' ? v.value() : v);
+        currBind = (typeof v.bind === 'function' ? v.bind() : undefined);
+        if(o[currValue] === undefined)
+        {
+          o[currValue] = {rows:[i],binds:[currBind]};
+        }
+        else
+        {
+          o[currValue].rows.push(i);
+          o[currValue].binds.push(currBind);
+        }
        return o;
       },{});
     }
 
-    Diff.diffString = function(o,n)
+    Diff.diffString = function(oldS,newS)
     {
-      var ns = Diff.convertWordArrToObj(n,'o'),
-          os = Diff.convertWordArrToObj(o,'n'),
-          i = 0,
-          valn = "",
-          valo = "",
-          isBind = false;
+      var ns = Diff.convertWordArrToObj(newS),
+          os = Diff.convertWordArrToObj(oldS),
+          n = [],
+          o = [],
+          t = [],
+          change = 'none';
 
       Object.keys(ns).forEach(function(k){
-        if ( ns[k].rows.length == 1 && typeof(os[k]) != "undefined" && os[k].rows.length == 1 ) {
-          isBind = (typeof n[ns[k].rows[0]] === 'string');
-          valn = (typeof n[ns[k].rows[0]] === 'string' ? n[ns[k].rows[0]] : n[ns[k].rows[0]].value());
-          valo = (typeof o[os[k].rows[0]] === 'string' ? o[os[k].rows[0]] : o[os[k].rows[0]].value());
-          n[ns[k].rows[0]] = { text: valn, row: os[k].rows[0], bind:(isBind ? n[ns[k].rows[0]] : undefined) };
-          o[os[k].rows[0]] = { text: valo, row: ns[k].rows[0], bind:(isBind ? o[os[k].rows[0]] : undefined) };
+        if(os[k] !== undefined)
+        {
+          ns[k].rows.forEach(function(d,x){
+            if(os[k].rows.indexOf(d) > -1)
+            {
+              change = 'none';
+              o.push({change:'none',pos:d,from:'n',text:k,bind:os[k].binds[os[k].rows.indexOf(d)]});
+              n.push({change:change,pos:d,from:'self',text:k,bind:os[k].binds[os[k].rows.indexOf(d)]});
+            }
+            else if(os[k].rows.indexOf(d+1) > -1)
+            {
+              change = 'add';
+              n.push({change:'delete',pos:d+1,from:'o',text:k,bind:os[k].binds[os[k].rows.indexOf(d+1)]});
+              o.push({change:'delete',pos:d+1,from:'self',text:k,bind:os[k].binds[os[k].rows.indexOf(d+1)]});
+              o.push({change:'add',pos:d,from:'n',text:k,bind:os[k].binds[os[k].rows.indexOf(d+1)]});
+              n.push({change:change,pos:d,from:'self',text:k,bind:os[k].binds[os[k].rows.indexOf(d+1)]});
+            }
+            else if(os[k].rows.indexOf(d-1) > -1)
+            {
+              change = 'add';
+              n.push({change:'delete',pos:d-1,from:'o',text:k,bind:os[k].binds[os[k].rows.indexOf(d-1)]});
+              o.push({change:'delete',pos:d-1,from:'self',text:k,bind:os[k].binds[os[k].rows.indexOf(d-1)]});
+              o.push({change:'add',pos:d,from:'n',text:k,bind:os[k].binds[os[k].rows.indexOf(d-1)]});
+              n.push({change:change,pos:d,from:'self',text:k,bind:os[k].binds[os[k].rows.indexOf(d-1)]});
+            }
+            else
+            {
+              change = 'add';
+              o.push({change:'add',pos:d,from:'n',text:k,bind:os[k].binds[os[k].rows.indexOf(d)]});
+              n.push({change:change,pos:d,from:'self',text:k,bind:os[k].binds[os[k].rows.indexOf(d)]});
+            }
+          });
+        }
+        else
+        {
+          ns[k].rows.forEach(function(d,x){
+            n.push({change:'add',pos:d,from:'self',text:k,bind:ns[k].binds[x]});
+            o.push({change:'add',pos:d,from:'n',text:k,bind:undefined});
+          });
         }
       });
+
+      Object.keys(os).forEach(function(k){
+        if(ns[k] !== undefined)
+        {
+          os[k].rows.forEach(function(d,x){
+            if(ns[k].rows.indexOf(d) > -1)
+            {
+              change = 'none';
+              n.push({change:'none',pos:d,from:'o',text:k,bind:os[k].binds[x]});
+            }
+            else if(ns[k].rows.indexOf(d+1) > -1)
+            {
+              change = 'delete';
+              n.push({change:'delete',pos:d,from:'o',text:k,bind:os[k].binds[x]});
+              n.push({change:'add',pos:(d+1),from:'self',text:k,bind:os[k].binds[x]});
+              o.push({change:'add',pos:(d+1),from:'n',text:k,bind:os[k].binds[x]});
+            }
+            else if(ns[k].rows.indexOf(d-1) > -1)
+            {
+              change = 'delete';
+              n.push({change:'delete',pos:d,from:'o',text:k,bind:os[k].binds[x]});
+              n.push({change:'add',pos:(d-1),from:'self',text:k,bind:os[k].binds[x]});
+              o.push({change:'add',pos:(d-1),from:'n',text:k,bind:os[k].binds[x]});
+            }
+            else
+            {
+              change = 'delete';
+              n.push({change:'delete',pos:d,from:'o',text:k,bind:os[k].binds[x]});
+            }
+            o.push({change:change,pos:d,from:'self',text:k,bind:os[k].binds[x]});
+          });
+        }
+        else
+        {
+          os[k].rows.forEach(function(d,x){
+            n.push({change:'delete',pos:d,text:k,bind:os[k].binds[x]});
+            o.push({change:'delete',pos:d,text:k,bind:undefined});
+          });
+        }
+      });
+
+        o = o.sort(function(a,b){
+          return (a.pos > b.pos ? 1 : -1);
+        });
+        n = n.sort(function(a,b){
+          return (a.pos > b.pos ? 1 : -1);
+        });
+        console.log(os,ns,o,n);
+      return;
+      /*
+        if ( ns[k].rows.length == 1 && typeof(os[k]) != "undefined" && os[k].rows.length == 1 ) {
+          isBindn = (typeof n[ns[k].rows[0].row] === 'string');
+          isBindo = (typeof o[os[k].rows[0].row] === 'string');
+          valn = (typeof n[ns[k].rows[0].row] === 'string' ? n[ns[k].rows[0].row] : n[ns[k].rows[0].row].value());
+          valo = (typeof o[os[k].rows[0].row] === 'string' ? o[os[k].rows[0].row] : o[os[k].rows[0].row].value());
+          n[ns[k].rows[0].row] = { text: valn, row: os[k].rows[0].row, bind:(isBindn ? n[ns[k].rows[0].row].bind : undefined) };
+          o[os[k].rows[0].row] = { text: valo, row: ns[k].rows[0].row, bind:(isBindo ? o[os[k].rows[0].row].bind : undefined) };
+        }
+      });*/
 
       for (i=0;i< n.length-1;i+=1)
       {
@@ -52,11 +158,12 @@ define([],function(){
       {
         if ( n[i].text != null && n[i-1].text == null && n[i].row > 0 && o[ n[i].row - 1 ].text == null && n[i-1] == o[ n[i].row - 1 ] )
         {
-          n[i-1] = { text: n[i-1], row: n[i].row - 1 };
+          n[i-1] = { text: n[i-1], row: n[i].row - 1};
           o[n[i].row-1] = { text: o[n[i].row-1], row: i - 1 };
         }
       }
       console.log(o,n);
+
       //no closures!!!
       return Array.prototype.concat.apply([],[o,n])
       .map(function(k,i){
