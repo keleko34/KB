86177b1e988bc50950cddf593e7f554be04778bc
keleko34 <gregory.guidero@gmail.com>
2016-09-07 17:15:15 +0200

    update gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,6 @@
 var gulp = require('gulp')
   , connect = require('./.gulp/server')
   , build = require('./.gulp/build');
-  /*
-  , connect = require('gulp-connect')
-  , prompt = require('gulp-prompt')
-  , inject = require('gulp-inject')
-  , sort = require('sort-stream')
-  , replace = require('gulp-replace')
-  , file = require('gulp-file')
-  , query = require('querystring')
-  , closureCompiler = require('gulp-closure-compiler')
-  , fs = require('fs');
-  */
-
-
-
 
 gulp.task('build', build());
 
