d2f1250f5c8925670dd012772913883d95fd4696
keleko34 <gregory.guidero@gmail.com>
2016-09-12 17:59:40 +0200

    start templates

--- KB/template.js
+++ KB/template.js
@@ -1,3 +1,31 @@
 define([],function(){
 
+  var _templates = {};
+
+  function getUnkownTemplates(template)
+  {
+
+  }
+
+  function templates()
+  {
+
+    return templates;
+  }
+
+  templates.isRegistered =  function(name)
+  {
+    return (_templates[name] !== undefined);
+  }
+
+  templates.register = function(name,template)
+  {
+
+  }
+
+  templates.getTemplate = function(name)
+  {
+
+  }
+
 });
